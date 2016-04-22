25323f3f848b8d4676f869b72c63339d68a591b6
Clay Branch <clay.branch@gmail.com>
2016-04-22 13:08:41 -0500

    reworked sendRequest to make it more testable and added tests

--- src/lib/sendRequest.js
+++ src/lib/sendRequest.js
@@ -1,6 +1,9 @@
+/* eslint no-console: 0 */
 import request from 'superagent'
 import entify from './entify'
 import createEventSource from './createEventSource'
+
+export default (opt) => (dispatch) => handleRequest(opt, dispatch, createEventSource, handleStandardRequest)
 
 export const prepareRequest = (opt, req) => {
   if (opt.headers) {
@@ -17,46 +20,48 @@ export const prepareRequest = (opt, req) => {
   }
 }
 
-export default (opt) => (dispatch) => {
+export const handleRequest = (opt, dispatch, onTail, onStandard) => {
   dispatch({
     type: 'tahoe.request',
     payload: opt
   })
+  opt.tail ? onTail(opt, dispatch) : onStandard(opt, dispatch, prepareRequest)
+}
 
-  if (opt.tail) {
-    return createEventSource(opt, dispatch)
-  }
-
-  const req = request[opt.method.toLowerCase()](opt.endpoint)
-  const debug = `${opt.method.toUpperCase()} ${opt.endpoint}`
-
-  prepareRequest(opt, req)
-
-  req.end((err, res) => {
-    if (!res && !err) {
-      err = new Error(`Connection failed: ${debug}`)
+export const handleSuccess = (res, opt, dispatch, getEntities) => {
+  if (opt.onResponse) opt.onResponse(res)
+  dispatch({
+    type: 'tahoe.success',
+    meta: opt,
+    payload: {
+      raw: res.body,
+      normalized: opt.model ? getEntities(res.body, opt) : null
     }
-    if (!err && res.type !== 'application/json') {
-      err = new Error(`Unknown response type: '${res.type}' from ${debug}`)
-    }
-    if (err) {
-      if (opt.onError) opt.onError(err)
-      return dispatch({
-        type: 'tahoe.failure',
-        meta: opt,
-        payload: err
-      })
-    }
-
-    // handle json responses
-    if (opt.onResponse) opt.onResponse(res)
-    dispatch({
-      type: 'tahoe.success',
-      meta: opt,
-      payload: {
-        raw: res.body,
-        normalized: opt.model ? entify(res.body, opt) : null
-      }
-    })
   })
+}
+
+export const handleError = (err, opt, dispatch) => {
+  if (opt.onError) opt.onError(err)
+  return dispatch({
+    type: 'tahoe.failure',
+    meta: opt,
+    payload: err
+  })
+}
+
+export const handleStandardRequest = (opt, dispatch, beforeEnd) => {
+  const req = request[opt.method.toLowerCase()](opt.endpoint)
+  beforeEnd(opt, req)
+  req.end((err, res) => handleResponse(err, res, opt, dispatch, handleError, handleSuccess))
+}
+
+export const handleResponse = (err, res, opt, dispatch, onError, onSuccess) => {
+  const debug = `${opt.method.toUpperCase()} ${opt.endpoint}`
+  if (!res && !err) {
+    err = new Error(`Connection failed: ${debug}`)
+  }
+  if (!err && res.type !== 'application/json') {
+    err = new Error(`Unknown response type: '${res.type}' from ${debug}`)
+  }
+  return err ? onError(err, opt, dispatch) : onSuccess(res, opt, dispatch, entify)
 }
--- test/sendRequestTest.js
+++ test/sendRequestTest.js
@@ -3,10 +3,19 @@
 
 import { fromJS } from 'immutable'
 import should from 'should'
-import sendRequest, { prepareRequest } from '../src/lib/sendRequest'
+import request from 'superagent'
+import sendRequest, { 
+  prepareRequest, 
+  handleResponse, 
+  handleSuccess, 
+  handleError,
+  handleRequest,
+  handleStandardRequest
+} from '../src/lib/sendRequest'
 import sinon from 'sinon'
+import entify from '../src/lib/entify'
 
-describe.only('sendRequest', () => {
+describe('sendRequest', () => {
   describe('the main function', () => {
     it('should exist', (done) => {
       should.exist(sendRequest)
@@ -47,4 +56,206 @@ describe.only('sendRequest', () => {
       done()
     })
   })
+  describe('the handleResponse helper', () => {
+    const dispatch = () => {}
+    const opt = {
+      method: 'get',
+      endpoint: '/test'
+    }
+    let successStub = null
+    let errorStub = null
+    beforeEach(() => {
+      successStub = sinon.stub()
+      errorStub = sinon.stub()
+    })
+    it('should call the error callback if an error is received', (done) => {
+      const err = new Error('Message')
+      const res = null
+      handleResponse(err, res, opt, dispatch, errorStub, successStub)
+      should(successStub.called).equal(false)
+      should(errorStub.args[0]).deepEqual([
+        err,
+        opt,
+        dispatch
+      ])
+      done()
+    })
+    it('should call the success callback if there are no errors', (done) => {
+      const err = null
+      const res = {
+        body: { test: 1 },
+        type: 'application/json'
+      }
+      handleResponse(err, res, opt, dispatch, errorStub, successStub)
+      should(errorStub.called).equal(false)
+      should(successStub.args[0]).deepEqual([
+        res,
+        opt,
+        dispatch,
+        entify
+      ])
+      done()
+    })
+    it('Should generate an error for non json responses', (done) => {
+      const err = null
+      const res = {
+        body: { test: 1 },
+        type: 'html'
+      }
+      handleResponse(err, res, opt, dispatch, errorStub, successStub)
+      should(successStub.called).equal(false)
+      should(errorStub.args[0]).deepEqual([
+        new Error("Unknown response type: 'html' from GET /test"),
+        opt,
+        dispatch
+      ])
+      done()
+    })
+    it('should generate an error failed connections', (done) => {
+      const err = null
+      const res = null
+      handleResponse(err, res, opt, dispatch, errorStub, successStub)
+      should(successStub.called).equal(false)
+      should(errorStub.args[0]).deepEqual([
+        new Error('Connection failed: GET /test'),
+        opt,
+        dispatch
+      ])
+      done()
+    })
+  })
+  describe('the response/request handlers', () => {
+    let dispatchStub = null
+    beforeEach(() => {
+      dispatchStub = sinon.stub()
+    })
+    describe('the handleSuccess helper', () => {
+      const res = {
+        body: {
+          test: 1
+        }
+      }
+      const opt = {
+        model: {},
+        onResponse: sinon.stub()
+      }
+      const getEntities = (body) => ({ entities: body })
+      it('should call onResponse from opt and dispatch a success action', (done) => {
+        handleSuccess(res, opt, dispatchStub, getEntities)
+        // the corrent action is dispatched
+        should(dispatchStub.args[0][0]).deepEqual({
+          type: 'tahoe.success',
+          meta: opt,
+          payload: {
+            raw: res.body,
+            normalized: { entities: { test: 1 } }
+          }
+        })
+
+        // the onResponse callback is called and the response is passed in
+        should(opt.onResponse.args[0][0]).deepEqual(res)
+        done()
+      })
+      it('should return null for normalized payload if no model is provided', (done) => {
+        const updatedOpt = fromJS(opt).delete('model').toJS()
+        handleSuccess(res, updatedOpt, dispatchStub, getEntities)
+        should(dispatchStub.args[0][0]).deepEqual({
+          type: 'tahoe.success',
+          meta: updatedOpt,
+          payload: {
+            raw: res.body,
+            normalized: null
+          }
+        })
+        done()
+      })
+    })
+    describe('the handleError helper', () => {
+      it('should call onError in opt and dispatch an error action', (done) => {
+        const err = new Error('test')
+        const opt = {
+          onError: sinon.stub()
+        }
+        handleError(err, opt, dispatchStub)
+        should(dispatchStub.args[0][0]).deepEqual({
+          type: 'tahoe.failure',
+          meta: opt,
+          payload: err
+        })
+        should(opt.onError.args[0][0]).deepEqual(err)
+        done()
+      })
+    })
+    describe('the handleRequest helper',  () => {
+      let tailStub = null
+      let standardStub = null
+      beforeEach(() => {
+        tailStub = sinon.stub()
+        standardStub = sinon.stub()
+      })
+      it('should dispatch the request action and call onStandard if tail.opt is not provided', (done) => {
+        const opt = {}
+        handleRequest(opt, dispatchStub, tailStub, standardStub)
+        should(dispatchStub.args[0][0]).deepEqual({
+          type: 'tahoe.request',
+          payload: opt
+        })
+        should(tailStub.called).equal(false)
+        should(standardStub.args[0]).deepEqual([
+          opt,
+          dispatchStub,
+          prepareRequest
+        ])
+        done()
+      })
+      it('should dispatch the request action and call onTail if tail.opt is provided', (done) => {
+        const opt = {
+          tail : true
+        }
+        handleRequest(opt, dispatchStub, tailStub, standardStub)
+        should(dispatchStub.args[0][0]).deepEqual({
+          type: 'tahoe.request',
+          payload: opt
+        })
+        should(standardStub.called).equal(false)
+        should(tailStub.args[0]).deepEqual([
+          opt,
+          dispatchStub
+        ])
+        done()
+      })
+
+    })
+    describe('the handleStandardRequest helper', () => {
+      it('should get a new request call beforeEnd and call request.end', (done) => {
+        const opt = {
+          method: 'GET',
+          endpoint: '/test'
+        }
+        const getRequestStub = sinon.stub(request, 'get')
+        const requestStub = {
+          end: sinon.stub()
+        }
+        const beforeEndStub = sinon.stub()
+        getRequestStub.returns(requestStub)
+        handleStandardRequest(opt, dispatchStub, beforeEndStub)
+        // should pass the point into the right request function
+        should(getRequestStub.args[0]).deepEqual([
+          '/test'
+        ])
+
+        // should pass opt and the request object into beforeEnd
+        should(beforeEndStub.args[0]).deepEqual([
+          opt,
+          requestStub
+        ])
+
+        // TODO: assert the actual contents of whats
+        // passed into end
+        should(requestStub.end.calledOnce).equal(true)
+        done()
+      })
+    })
+  })
+
 })
